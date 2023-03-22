a8c8b3e9d0692eea8b5e22dbb9ee720b477b8e42
Sam Joel <samjoel3101@gmail.com>
2023-03-22 16:06:36 +0530

    Remove post build labelling on cog build

--- pkg/image/build.go
+++ pkg/image/build.go
@@ -1,8 +1,8 @@
 package image
 
 import (
-	"bytes"
-	"encoding/json"
+	// "bytes"
+	// "encoding/json"
 	"fmt"
 	"os"
 	"os/exec"
@@ -11,7 +11,7 @@ import (
 	"github.com/replicate/cog/pkg/config"
 	"github.com/replicate/cog/pkg/docker"
 	"github.com/replicate/cog/pkg/dockerfile"
-	"github.com/replicate/cog/pkg/global"
+	// "github.com/replicate/cog/pkg/global"
 	"github.com/replicate/cog/pkg/util/console"
 )
 
@@ -40,59 +40,43 @@ func Build(cfg *config.Config, dir, imageName string, secrets []string, noCache 
 		return fmt.Errorf("Failed to build Docker image: %w", err)
 	}
 
-	console.Info("Adding labels to image...")
-	schema, err := GenerateOpenAPISchema(imageName, cfg.Build.GPU)
-	if err != nil {
-		return fmt.Errorf("Failed to get type signature: %w", err)
-	}
-	configJSON, err := json.Marshal(cfg)
-	if err != nil {
-		return fmt.Errorf("Failed to convert config to JSON: %w", err)
-	}
-	// We used to set the cog_version and config labels in Dockerfile, because we didn't require running the
-	// built image to get those. But, the escaping of JSON inside a label inside a Dockerfile was gnarly, and
-	// doesn't seem to be a problem here, so do it here instead.
-	labels := map[string]string{
-		global.LabelNamespace + "version": global.Version,
-		global.LabelNamespace + "config":  string(bytes.TrimSpace(configJSON)),
-		// Mark the image as having an appropriate init entrypoint. We can use this
-		// to decide how/if to shim the image.
-		global.LabelNamespace + "has_init": "true",
-		// Backwards compatibility. Remove for 1.0.
-		"org.cogmodel.deprecated":  "The org.cogmodel labels are deprecated. Use run.cog.",
-		"org.cogmodel.cog_version": global.Version,
-		"org.cogmodel.config":      string(bytes.TrimSpace(configJSON)),
-	}
+	// console.Info("Adding labels to image...")
+	// schema, err := GenerateOpenAPISchema(imageName, cfg.Build.GPU)
+	// if err != nil {
+	// 	return fmt.Errorf("Failed to get type signature: %w", err)
+	// }
+	// configJSON, err := json.Marshal(cfg)
+	// if err != nil {
+	// 	return fmt.Errorf("Failed to convert config to JSON: %w", err)
+	// }
+	// // We used to set the cog_version and config labels in Dockerfile, because we didn't require running the
+	// // built image to get those. But, the escaping of JSON inside a label inside a Dockerfile was gnarly, and
+	// // doesn't seem to be a problem here, so do it here instead.
+	// labels := map[string]string{
+	// 	global.LabelNamespace + "version": global.Version,
+	// 	global.LabelNamespace + "config":  string(bytes.TrimSpace(configJSON)),
+	// 	// Mark the image as having an appropriate init entrypoint. We can use this
+	// 	// to decide how/if to shim the image.
+	// 	global.LabelNamespace + "has_init": "true",
+	// 	// Backwards compatibility. Remove for 1.0.
+	// 	"org.cogmodel.deprecated":  "The org.cogmodel labels are deprecated. Use run.cog.",
+	// 	"org.cogmodel.cog_version": global.Version,
+	// 	"org.cogmodel.config":      string(bytes.TrimSpace(configJSON)),
+	// }
 
-	// OpenAPI schema is not set if there is no predictor.
-	if len((*schema).(map[string]interface{})) != 0 {
-		schemaJSON, err := json.Marshal(schema)
-		if err != nil {
-			return fmt.Errorf("Failed to convert type signature to JSON: %w", err)
-		}
-		labels[global.LabelNamespace+"openapi_schema"] = string(schemaJSON)
-		labels["org.cogmodel.openapi_schema"] = string(schemaJSON)
-	}
+	// // OpenAPI schema is not set if there is no predictor.
+	// if len((*schema).(map[string]interface{})) != 0 {
+	// 	schemaJSON, err := json.Marshal(schema)
+	// 	if err != nil {
+	// 		return fmt.Errorf("Failed to convert type signature to JSON: %w", err)
+	// 	}
+	// 	labels[global.LabelNamespace+"openapi_schema"] = string(schemaJSON)
+	// 	labels["org.cogmodel.openapi_schema"] = string(schemaJSON)
+	// }
 
-	commit, err := gitHead(dir)
-	if err != nil {
-		console.Warnf("Failed to determine Git commit: %s", err)
-	}
-	if commit != "" {
-		labels["org.opencontainers.image.revision"] = commit
-	}
-
-	tag, err := gitTag(dir)
-	if err != nil {
-		console.Warnf("Failed to determine Git tag: %s", err)
-	}
-	if tag != "" {
-		labels["org.opencontainers.image.version"] = tag
-	}
-
-	if err := docker.BuildAddLabelsToImage(imageName, labels); err != nil {
-		return fmt.Errorf("Failed to add labels to image: %w", err)
-	}
+	// if err := docker.BuildAddLabelsToImage(imageName, labels); err != nil {
+	// 	return fmt.Errorf("Failed to add labels to image: %w", err)
+	// }
 	return nil
 }
 
